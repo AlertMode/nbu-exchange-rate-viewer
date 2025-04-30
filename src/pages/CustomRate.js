10a0af1766fcbec5a9a990c7f8d6226691c7024a
Oleksii_Borovykov <aleksaybor@gmail.com>
2025-04-30 21:57:32 +0300

    Updated project to use React 19.0.0 and updated package.json accordingly. Also, small project restructuring: - Moved cloneArray.js to src/helpers - Moved fetchDataFromJSON.js to src/requests

--- src/pages/CustomRate.js
+++ src/pages/CustomRate.js
@@ -2,15 +2,14 @@ import React from 'react'
 import { useStore } from '../store'
 
 const CustomRate = () => {
+  const { rates } = useStore()
 
-    const { rates } = useStore()
-
-    return (
-        <div>
-            <h2>Changed Exchange Rates</h2>
-            {console.log(rates)}
-        </div>
-    )
+  return (
+    <div>
+      <h2>Changed Exchange Rates</h2>
+      {console.log(rates)}
+    </div>
+  )
 }
 
 export default CustomRate
